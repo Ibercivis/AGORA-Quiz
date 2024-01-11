d30abffcf2e8175e7e8c755579ec95ca16649339
jbarbaborderias <jbarba@ibercivis.es>
2024-01-11 14:49:55 +0000

    GameService already connected

--- classic_game/api/serializers.py
+++ classic_game/api/serializers.py
@@ -4,11 +4,11 @@ from classic_game.models import Question, Game
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ['id', 'question_text', 'answer1', 'answer2', 'answer3', 'answer4']
+        fields = ['id', 'question_text', 'answer1', 'answer2', 'answer3', 'answer4', 'correct_answer']
 
 class GameSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Game
-        fields = ['id', 'player', 'questions', 'score', 'created_at']
+        fields = ['id', 'player', 'questions', 'score', 'status', 'created_at']
--- classic_game/api/views.py
+++ classic_game/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
+from django.http import Http404
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -7,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Question, Game
+from classic_game.models import Question, Game
 from .serializers import QuestionSerializer, GameSerializer  
 
 import random
@@ -22,6 +23,15 @@ class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
 
+    def calculate_current_question_index(self, game):
+        # Calcula el índice de la pregunta actual
+        answered_questions_count = len(game.responses)
+        return answered_questions_count + 1  # +1 ya que el índice debería comenzar desde 1
+
+    def calculate_correct_answers_count(self, game):
+        # Cuenta el número de respuestas correctas
+        return sum(1 for response in game.responses if response.get('correct', False))
+
     @action(detail=False, methods=['post'])
     def start_game(self, request, *args, **kwargs):
         # Verificar si ya existe una partida en progreso
@@ -34,7 +44,22 @@ class GameViewSet(viewsets.ModelViewSet):
         game.questions.set(questions)
         game.status = 'in_progress'
         game.save()
-        return Response(self.get_serializer(game).data)
+        # Encuentra la próxima pregunta que no ha sido respondida
+        next_question = None
+        for question in game.questions.all():
+            if not any(r['question_id'] == question.id for r in game.responses):
+                next_question = question
+                break
+        #return Response(self.get_serializer(game).data)
+        response_data = {
+        'game': GameSerializer(game).data,
+        'score': game.score,
+        'status': game.status,
+        'next_question': QuestionSerializer(next_question).data if next_question else None,
+        'current_question_index': 1,
+        'correct_answers_count': 0,
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['post'])
     def answer(self, request, pk=None):
@@ -74,17 +99,62 @@ class GameViewSet(viewsets.ModelViewSet):
         remaining_questions = game.questions.exclude(id__in=[r['question_id'] for r in game.responses])
         remaining_questions_data = QuestionSerializer(remaining_questions, many=True).data
 
-        # Devolver la respuesta incluyendo el estado de la partida
-        return Response({
-            'correct': correct,
-            'score': game.score,
-            'remaining_questions': remaining_questions_data,
-            'status': game.status  # Incluye el estado de la partida
-        }, status=status.HTTP_200_OK)
+        # Encuentra la próxima pregunta que no ha sido respondida
+        next_question = None
+        for question in game.questions.all():
+            if not any(r['question_id'] == question.id for r in game.responses):
+                next_question = question
+                break
 
-        @action(detail=True, methods=['post'])
-        def abandon_game(self, request, pk=None):
-            game = self.get_object()
-            game.status = 'incomplete'
-            game.save()
-            return Response({'status': 'Game marked as incomplete.'}, status=status.HTTP_200_OK)
+        # Calcular el índice de la pregunta actual
+        current_question_index = self.calculate_current_question_index(game)
+
+        # Calcular el número de respuestas acertadas
+        correct_answers_count = self.calculate_correct_answers_count(game)
+
+        # Devolver la respuesta
+        response_data = {
+        'correct': correct,
+        'score': game.score,
+        'status': game.status,
+        'next_question': QuestionSerializer(next_question).data if next_question else None,
+        'current_question_index': current_question_index,
+        'correct_answers_count': correct_answers_count,
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'])
+    def resume(self, request):
+        # Busca un juego en progreso para el usuario actual
+        game = Game.objects.filter(player=request.user, status='in_progress').first()
+        if not game:
+            return Response({'message': 'No hay juego en progreso.'}, status=status.HTTP_200_OK)
+
+        # Encuentra la próxima pregunta que no ha sido respondida
+        next_question = None
+        for question in game.questions.all():
+            if not any(r['question_id'] == question.id for r in game.responses):
+                next_question = question
+                break
+
+        if next_question is None:
+            raise Http404("No hay más preguntas o el juego está completo.")
+        
+        current_question_index = self.calculate_current_question_index(game)
+        # Calcular el número de respuestas acertadas
+        correct_answers_count = self.calculate_correct_answers_count(game)
+
+        response_data = {
+            'game': GameSerializer(game).data,
+            'next_question': QuestionSerializer(next_question).data,
+            'current_question_index': current_question_index,
+            'correct_answers_count': correct_answers_count,
+        }
+        return Response(response_data)
+
+    @action(detail=True, methods=['post'])
+    def abandon_game(self, request, pk=None):
+        game = self.get_object()
+        game.status = 'incomplete'
+        game.save()
+        return Response({'status': 'Game marked as incomplete.'}, status=status.HTTP_200_OK)
