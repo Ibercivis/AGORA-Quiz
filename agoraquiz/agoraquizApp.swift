f65c3e618477f2c8eca23ca65269117da66ba823
jbarbaborderias <jbarba@bifi.es>
2024-05-07 12:04:00 +0200

    Initial Commit

---
+++ agoraquiz/agoraquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  agoraquizApp.swift
+//  agoraquiz
+//
+//  Created by Ibercivis on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct agoraquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
