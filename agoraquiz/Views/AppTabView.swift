dc86ee8820f6bf453397306f0f588a1ee7ffc310
jbarbaborderias <jbarba@bifi.es>
2024-05-13 20:09:53 +0200

    MainView and Tab navigation

---
+++ agoraquiz/Views/AppTabView.swift
@@ -0,0 +1,18 @@
+//
+//  AppTabView.swift
+//  agoraquiz
+//
+//  Created by Ibercivis on 13/5/24.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppTabView()
+}
