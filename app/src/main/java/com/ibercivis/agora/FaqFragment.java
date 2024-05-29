ba6aa3c8c9ede1d6b40a42351b30525bc525b39d
jbarbaborderias <jbarba@bifi.es>
2024-05-29 12:30:01 +0200

    Beta version working

--- app/src/main/java/com/ibercivis/agora/FaqFragment.java
+++ app/src/main/java/com/ibercivis/agora/FaqFragment.java
@@ -38,12 +38,13 @@ public class FaqFragment extends Fragment {
 
     private List<FaqItem> getFaqItems() {
         List<FaqItem> items = new ArrayList<>();
-        items.add(new FaqItem("How do I reset my password?", "To reset your password, go to the login screen and click on 'Forgot password'. Follow the instructions to reset your password."));
-        items.add(new FaqItem("How do I create an account?", "To create an account, click on 'Sign up' on the login screen and fill in the required details."));
-        items.add(new FaqItem("How can I contact technical support?", "You can contact technical support by emailing support@agora.com or calling our helpline."));
+        items.add(new FaqItem("How can I improve my scores?", "Within the AGORA project, we have developed two Academies where you can find information and resources to increase your knowledge and improve your scores."));
+        items.add(new FaqItem("How can I change my password?", "To reset your password, go to the login screen and click on 'Forgot password?' or the Edit Profile section. Follow the instructions sent to your email."));
+        items.add(new FaqItem("How can I report a bug?", "The app is currently in beta mode, so it is normal for some issues to occur. If you find something that is not working properly, please contact us at info@ibercivis.es"));
+        items.add(new FaqItem("How can I contact technical support?", "You can contact technical support by emailing info@ibercivis.es"));
         items.add(new FaqItem("Why are the Categories and Multiplayer game modes not available?", "The app will continue to be developed during the life of the project, so new functionalities will be integrated in future releases."));
-        items.add(new FaqItem("How can I edit my profile?", "To edit your profile, go to the settings page and click on 'Edit Profile'."));
-        items.add(new FaqItem("Where can I read about the Agora Project?", "You can read about the Agora Project on our official website."));
+        items.add(new FaqItem("How can I edit my profile?", "To edit your profile, go to the profile screen and click on the 'Edit Profile' button."));
+        items.add(new FaqItem("Where can I read about the Agora Project?", "You can find more information about the AGORA project on our website and social media channels. All relevant links are available in the About Us - Contact Us section."));
         return items;
     }
 }
